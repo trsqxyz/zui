21a45b494107c29ccc0f7def4acf52103e2965f2
trsqxyz <trsqxyz@gmail.com>
2015-05-25 21:18:50 +0900

    Update: This isn't able to use DayOne URL scheme. It just push_note to new device on Pushbullet that create by Zui. No link to DayOne, just push to Pushbullet.

--- zui.py
+++ zui.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import platform
+import sys
 import webbrowser
 
 import config
@@ -8,47 +11,89 @@ from pushbullet import Pushbullet
 
 
 class Zui:
+
     def __init__(self):
-        if not config.API_KEY:
+        self.pb = Pushbullet(self.api_key())
+        self.target = self.make_devices()
+        self.dayone = config.URL_SCHEME
+
+    def api_key(self):
+        if config.API_KEY:
+            return config.API_KEY
+        else:
             webbrowser.open('https://www.pushbullet.com/account')
             API_KEY = input('Copy and Paste Access Token: ')
-            with open('config.py', 'r') as rf:
-                setting = rf.readlines()
-                setting[0] = 'API_KEY = "{0}"\n'.format(API_KEY)
-            with open('config.py', 'w') as wf:
-                wf.writelines(setting)
-                wf.flush()
-        else:
-            API_KEY = config.API_KEY
-        self._name = config.PUSH_TARGET
-        self.pb = Pushbullet(API_KEY)
-        self.make_devices()
-        self.dayone = config.URL_SCHEME
+            self.config_setting(API_KEY)
+            return API_KEY
+
+    def config_setting(self, api_key):
+        with open('config.py', 'r') as rf:
+            setting = rf.readlines()
+            setting[0] = 'API_KEY = "{0}"\n'.format(api_key)
+        with open('config.py', 'w') as wf:
+            wf.writelines(setting)
+            wf.flush()
 
     def make_devices(self):
         for d in self.pb.devices:
             if config.PUSH_TARGET == d.nickname:
-                self.target = d
-                break
+                return d
             else:
                 new_device = self.pb.new_device(config.PUSH_TARGET)
                 #  model argument was not used, only nickname
                 self.pb.edit_device(
-                                    new_device,
-                                    nickname=self._name,
-                                    model=config.PUSH_TARGET
-                                    )
+                    new_device,
+                    nickname=config.PUSH_TARGET,
+                    model=config.PUSH_TARGET
+                )
                 self.make_devices()
 
     def push_to_dayone(self):
-        body = input('Write:\n').strip()
-        body = self.dayone + body
-        push = self.pb.push_note('', body, device=self.target)
+        '''Pushbullet couldn't link then whitespace in URL.
+        So, it doesn't push_link, just push_note.
+        Unavilable DayOne URL shceme.
+        '''
+        try:
+            #  body = self.dayone + self.notepad()
+            body = self.notepad()
+            return self.pb.push_note('', body, device=self.target)
+        except (KeyboardInterrupt, TypeError) as e:
+            return False
+        finally:
+            self.clear_notepad()
+
+    def notepad(self):
+        try:
+            pause = self.clear_notepad()
+            print('Push: {}, Close: C-c'.format(pause))
+            lines = [line for line in sys.stdin.readlines()]
+            return ''.join(lines)
+        except KeyboardInterrupt as e:
+            return e
+
+    def clear_notepad(self):
+        clear = {
+            'Windows': (
+                'CLS',
+                'C-z'
+            ),
+            'Darwin': (
+                'clear',
+                'C-d'
+            ),
+        }
+        clear, pause = clear[platform.system()][0], clear[platform.system()][1]
+        os.system(clear)
+        return pause
 
 
 def main():
     z = Zui()
-    z.push_to_dayone()
+    while z.push_to_dayone():
+        pass
+    else:
+        print('Bye')
+
 
 if __name__ == '__main__':
     main()
